3b4c02a9b3b52ea690b91937980f4fbe96e2e8dc
Nada0795 <nadakh1993@icloud.com>
2021-08-02 14:01:14 +0300

    added map

--- src/App.js
+++ src/App.js
@@ -13,14 +13,18 @@ class App extends React.Component{
     super(props);
     this.state={
      disName :'',
-    
+     lat :'',
+     lon :'',
+     showMap:false,
+     errMsg: 'Unable to geocode',
+     displayErr:false
     }
   }
   getLocData = async(event)=>{
     event.preventDefault();
    let cityName= event.target.city.value;
   let URL=`https://us1.locationiq.com/v1/search.php?key=pk.b2d9bf0052a0428c7de26a517415f6f9&q=${cityName}&format=json`;
-
+  try{
   let locResult = await axios.get(URL);  
    this.setState({
      disName : locResult.data[0].display_name,
@@ -28,7 +32,12 @@ class App extends React.Component{
      lon: locResult.data[0].lon,
      showMap: true,
    })
-
+ }
+  catch{
+    this.setState({
+      displayErr:true,
+    })
+  }
 }
   render(){
     return(
@@ -44,7 +53,7 @@ class App extends React.Component{
   <Row className="align-items-center">
     <Col sm={3} className="my-1">
       <Form.Label htmlFor="inlineFormInputName" visuallyHidden>
-        Name
+        Country Name
       </Form.Label>
       <Form.Control id="inlineFormInputName" placeholder="Enter City" name='city' />
     </Col>
@@ -56,7 +65,12 @@ class App extends React.Component{
 <p>
 {this.state.disName}
 </p>
-
+{
+  this.state.showMap && <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.b2d9bf0052a0428c7de26a517415f6f9&center=${this.state.lat},${this.state.lon}& zoom=18`} alt="map"/>
+}
+{
+  this.state.displayErr && this.state.errMsg
+}
 <p>
 &copy;NadaAl-abdullah
 </p>
